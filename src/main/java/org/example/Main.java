8e113806a86a71e745a8c17c38c7e436baacf18d
Brinder <brinderamirez08@gmail.com>
2024-12-15 15:23:02 -0500

    Ejercicio 3 avance

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,14 @@ import org.example.ejercicio1.Betamon;
 import org.example.ejercicio1.Digimon;
 import org.example.ejercicio1.Gabumon;
 
+import java.util.List;
+import java.util.function.Function;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args){
+        //Ejercicio 1
         Digimon Agumon= new Agumon("Carlos",13,7.9,true,"Dragones");
         Digimon Gabumon= new Gabumon("Roberto", 15,17.8,false,7);
         Digimon Betamon=  new Betamon("Gonzalo",22,26.7,true,45.8);
@@ -21,6 +25,12 @@ public class Main {
         System.out.println("---------------------");
         System.out.println(Betamon.toString());
         Betamon.evolucion();
-    }
 
+
+        //Ejercicio 3
+//        List<Integer> integerList = List.of(1,2,3,4,5,6,7,8);
+//        Function<List<Integer>, Integer> suma = list -> list.stream()  // Convertimos la lista en un stream
+//                .reduce(0, Integer::sum);  // Sumamos los elementos
+//        System.out.println(suma.apply(integerList));
+    }
 }
